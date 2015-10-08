82c4995eccc3cca8f2a0854d7b49d1c46c9a9ea3
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-08 23:47:48 +0300

    fix(js): bind new tooltips on ajax success

--- views/default/components/tooltip.js
+++ views/default/components/tooltip.js
@@ -1,8 +1,14 @@
-require(['elgg', 'jquery'], function (elgg, $) {
-	if ($('.elgg-tooltip').length) {
-		require(['jquery', 'components/classes/tooltip'], function ($, Tooltip) {
-			var tp = new Tooltip($('.elgg-tooltip'));
-			tp.init();
-		});
+require(['jquery'], function ($) {
+
+	function init() {
+		if ($('.elgg-tooltip').length) {
+			require(['jquery', 'components/classes/tooltip'], function ($, Tooltip) {
+				var tp = new Tooltip($('.elgg-tooltip'));
+				tp.init();
+			});
+		}
 	}
+
+	init();
+	$(document).ajaxComplete(init);
 });
